a8a79c78140dc6aab12a3db35336100964454e60
John-A-Aydin <johnaaydin@gmail.com>
2024-12-16 08:10:13 -0500

    Day 03 Part 02

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -1,7 +1,11 @@
+use std::fs;
+
 mod soln;
 
 fn main() {
-    let part_1_ans = soln::solve();
+    let data = fs::read_to_string("data/data.txt").expect("Could not find data");
+    let part_1_ans = soln::solve_p1(data.clone());
     println!("Part 1 Answer: {}", part_1_ans);
-    println!("Hello, world!");
+    let part_2_ans = soln::solve_p2(data.clone());
+    println!("Part 2 Answer: {}", part_2_ans);
 }
--- day-03/src/soln.rs
+++ day-03/src/soln.rs
@@ -1,26 +1,44 @@
 use regex::Regex;
-use std::fs;
 
-pub fn solve() -> i32 {
-    let contents = fs::read_to_string("data/data.txt").expect("Could not find data");
-
+pub fn solve_p1(data: String) -> i32 {
     let re = Regex::new(r"mul\([0-9]{1,3},[0-9]{1,3}\)").unwrap();
     let funcs: Vec<&str> = re
-        .captures_iter(&contents)
+        .captures_iter(&data)
         .map(|captures| captures.get(0).unwrap().as_str())
         .collect();
-    let ans: i32 = funcs
-        .into_iter()
-        .map(|s| {
-            let start = s.char_indices().nth(4).unwrap().0;
-            let end = s.char_indices().rev().nth(0).unwrap().0;
-            let result = &s[start..end];
-            println!("{}", s);
-            return result.split(',').fold(1, |acc, val| {
-                println!("{}", val);
-                return acc * val.parse::<i32>().unwrap();
-            });
+    let ans: i32 = funcs.into_iter().map(|s| calculate_mul(s)).sum();
+    return ans;
+}
+
+pub fn solve_p2(data: String) -> i32 {
+    let re = Regex::new(r"(mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don't\(\))").unwrap();
+
+    let mut should_do = true;
+
+    let ans: i32 = re
+        .captures_iter(&data)
+        .map(|captures| {
+            let s = captures.get(0).unwrap().as_str();
+            match s {
+                "do()" => should_do = true,
+                "don't()" => should_do = false,
+                _ => {
+                    if should_do {
+                        return calculate_mul(s);
+                    }
+                }
+            }
+            return 0;
         })
         .sum();
     return ans;
+}
+
+fn calculate_mul(f: &str) -> i32 {
+    let start = f.char_indices().nth(4).unwrap().0;
+    let end: usize = f.char_indices().rev().nth(0).unwrap().0;
+    let result = &f[start..end];
+    return result.split(',').fold(1, |acc, val| {
+        return acc * val.parse::<i32>().unwrap();
+    });
 }
