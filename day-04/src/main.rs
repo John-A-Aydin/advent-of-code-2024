c550cb9b1b6752334826990c488fa817e0d10f1e
John-A-Aydin <johnaaydin@gmail.com>
2024-12-17 15:47:20 -0500

    Day 04 Part 02

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -6,4 +6,6 @@ fn main() {
     let data = fs::read_to_string("data/data.txt").expect("Could not find data");
     let part_1_ans = soln::solve_p1(&data);
     println!("Part 1 Answer: {}", part_1_ans);
+    let part_2_ans = soln::solve_p2(&data);
+    println!("Part 2 Answer: {}", part_2_ans);
 }
--- day-04/src/soln.rs
+++ day-04/src/soln.rs
@@ -1,11 +1,6 @@
 pub fn solve_p1(data: &str) -> i32 {
     // Parse into matrix of chars
-    let mat: Vec<Vec<char>> = data
-        .split('\n')
-        .map(|line| {
-            return line.chars().collect();
-        })
-        .collect();
+    let mat = str_to_matrix(data);
 
     let ans = mat.iter().enumerate().fold(0, |acc_y: i32, (i, row)| {
         return acc_y
@@ -17,18 +12,35 @@ pub fn solve_p1(data: &str) -> i32 {
     return ans;
 }
 
+pub fn solve_p2(data: &str) -> i32 {
+    let mat = str_to_matrix(data);
+    let ans = mat.iter().enumerate().fold(0, |acc_y: i32, (i, row)| {
+        return acc_y
+            + row.iter().enumerate().fold(0, |acc_x, (j, _)| {
+                return acc_x + check_from_middle(i, j, &mat);
+            });
+    });
+    return ans;
+}
+
+fn str_to_matrix(data: &str) -> Vec<Vec<char>> {
+    return data
+        .split('\n')
+        .map(|line| {
+            return line.chars().collect();
+        })
+        .collect();
+}
+
+fn get_nested_value<T>(m: &Vec<Vec<T>>, row: usize, col: usize) -> Option<&T> {
+    m.get(row)?.get(col)
+}
+
 fn check_from_start(i: usize, j: usize, mat: &Vec<Vec<char>>) -> i32 {
     let xmas = ['X', 'M', 'A', 'S'];
 
-    let start = get_nested_value(mat, i, j);
-
-    match start {
-        Some(s) => {
-            if *s != 'X' {
-                return 0;
-            }
-        }
-        None => return 0,
+    if !check_nested_value(mat, i, j, 'X') {
+        return 0;
     }
 
     let mut ans = 0;
@@ -41,11 +53,12 @@ fn check_from_start(i: usize, j: usize, mat: &Vec<Vec<char>>) -> i32 {
                 if row_idx < 0 || col_idx < 0 {
                     break;
                 }
-                let x = get_nested_value(mat, row_idx as usize, col_idx as usize);
-                if x.is_none() {
-                    break;
-                }
-                if *x.unwrap() != xmas[char_idx as usize] {
+                if !check_nested_value(
+                    mat,
+                    row_idx as usize,
+                    col_idx as usize,
+                    xmas[char_idx as usize],
+                ) {
                     break;
                 }
                 if char_idx == 3 {
@@ -58,6 +71,44 @@ fn check_from_start(i: usize, j: usize, mat: &Vec<Vec<char>>) -> i32 {
     return ans;
 }
 
-fn get_nested_value<T>(m: &Vec<Vec<T>>, row: usize, col: usize) -> Option<&T> {
-    m.get(row)?.get(col)
+fn check_from_middle(i: usize, j: usize, mat: &Vec<Vec<char>>) -> i32 {
+    // make sure i, j != 0
+    if i < 1 || j < 1 {
+        return 0;
+    }
+    if !check_nested_value(mat, i, j, 'A') {
+        return 0;
+    }
+
+    // top left - bottom right check
+    let tl_br = (check_nested_value(mat, i - 1, j - 1, 'M')
+        && check_nested_value(mat, i + 1, j + 1, 'S'))
+        || (check_nested_value(mat, i - 1, j - 1, 'S')
+            && check_nested_value(mat, i + 1, j + 1, 'M'));
+
+    // top right - bottom left check
+    let tr_bl = (check_nested_value(mat, i + 1, j - 1, 'M')
+        && check_nested_value(mat, i - 1, j + 1, 'S'))
+        || (check_nested_value(mat, i + 1, j - 1, 'S')
+            && check_nested_value(mat, i - 1, j + 1, 'M'));
+
+    return (tl_br && tr_bl) as i32;
+}
+
+fn check_nested_value<T: std::cmp::PartialEq>(
+    m: &Vec<Vec<T>>,
+    row: usize,
+    col: usize,
+    target: T,
+) -> bool {
+    let val = get_nested_value(m, row, col);
+    match val {
+        Some(v) => {
+            if *v == target {
+                return true;
+            }
+            return false;
+        }
+        None => return false,
+    }
 }
