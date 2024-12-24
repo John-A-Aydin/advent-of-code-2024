108800fb70f971897cb4b4a073f91a9b13997ec6
John-A-Aydin <johnaaydin@gmail.com>
2024-12-24 11:36:26 -0500

    Day 07 Part 02

--- day-07/src/soln.rs
+++ day-07/src/soln.rs
@@ -1,16 +1,54 @@
-pub fn solve_p1(data: &str) -> u64 {
-    let mut sum = 0;
-
-    // F it we going recursive
-    fn rec(operands: &[u64], idx: usize, test_value: u64, current_value: u64) -> bool {
-        // return rec(operands, op)
+pub fn solve(data: &str) -> (u64, u64) {
+    /// No idea what to call this
+    /// enter with idx = 0, current_value = None
+    fn rec(operands: &[u64], idx: usize, test_value: u64, current_value: Option<u64>) -> bool {
+        if idx == 0 || current_value.is_none() {
+            return rec(operands, 1, test_value, Some(operands[0]));
+        }
         // Recursion goes past the length of operands by 1
         if idx >= operands.len() {
-            return current_value == test_value;
+            return current_value.unwrap() == test_value;
         }
-        return rec(operands, idx + 1, test_value, current_value + operands[idx])
-            || rec(operands, idx + 1, test_value, current_value * operands[idx]);
+        return rec(
+            operands,
+            idx + 1,
+            test_value,
+            Some(current_value.unwrap() + operands[idx]),
+        ) || rec(
+            operands,
+            idx + 1,
+            test_value,
+            Some(current_value.unwrap() * operands[idx]),
+        );
     }
+
+    /// No idea what to call this
+    /// enter with idx = 0, current_value = None
+    fn rec2(operands: &[u64], idx: usize, test_value: u64, current_value: Option<u64>) -> bool {
+        // Recursion goes past the length of operands by 1
+        if idx == 0 || current_value.is_none() {
+            return rec2(operands, 1, test_value, Some(operands[0]));
+        }
+        if idx >= operands.len() {
+            return current_value.unwrap() == test_value;
+        }
+        let concat_value =
+            current_value.unwrap() * 10u64.pow(operands[idx].ilog10() + 1) + operands[idx];
+        return rec2(
+            operands,
+            idx + 1,
+            test_value,
+            Some(current_value.unwrap() + operands[idx]),
+        ) || rec2(
+            operands,
+            idx + 1,
+            test_value,
+            Some(current_value.unwrap() * operands[idx]),
+        ) || rec2(operands, idx + 1, test_value, Some(concat_value));
+    }
+
+    let mut p1_ans = 0;
+    let mut p2_ans = 0;
 
     data.split('\n').for_each(|line| {
         let eqn: Vec<&str> = line.split(": ").collect();
@@ -20,9 +58,13 @@ pub fn solve_p1(data: &str) -> u64 {
             .map(|operand_str| operand_str.parse::<u64>().expect("non-numeric input"))
             .collect();
 
-        if rec(&operands.as_slice(), 1, test_value, operands[0]) {
-            sum += test_value;
+        if rec(&operands.as_slice(), 0, test_value, None) {
+            p1_ans += test_value;
+        }
+        if rec2(&operands.as_slice(), 0, test_value, None) {
+            p2_ans += test_value;
         }
     });
-    return sum;
+
+    return (p1_ans, p2_ans);
 }
